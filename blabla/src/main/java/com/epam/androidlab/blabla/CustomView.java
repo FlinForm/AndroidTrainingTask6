1b7285ecc1d1f9b1439d19f94eda3ed9332177d3
pavel <FlinForm@gmail.com>
2017-05-20 21:16:47 +0300

    Commit 1: Task completed

--- blabla/src/main/java/com/epam/androidlab/blabla/CustomView.java
+++ blabla/src/main/java/com/epam/androidlab/blabla/CustomView.java
@@ -9,7 +9,6 @@ import android.graphics.Path;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 public class CustomView extends View implements View.OnClickListener {
